cc44c29e32f4ea850f91d0a99d1d91597501aad6
Maksym Kundeev <mkundeev@gmail.com>
2022-06-15 17:23:16 +0300

    resetForm

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -14,10 +14,10 @@ render() {
          name: Yup.string().matches(/(^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$)/,"Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan").required('Required'),
          number:  Yup.string().matches(/(\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9})/,"Phone number must be digits and can contain spaces, dashes, parentheses and can start with +").required('Required'),
        })}
-       onSubmit={(values, { setSubmitting }) => {
+       onSubmit={(values, { setSubmitting, resetForm }) => {
             this.props.onSubmit(values)
            setSubmitting(false);
-         
+          resetForm({})
        }}
      >
        <Form className={s.form}>
